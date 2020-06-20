c7c30bcc7bc629a83a86ce1662cebd704957fe35
src-thk <64914515+src-thk@users.noreply.github.com>
2020-06-21 00:14:30 +0530

    Update WhatsAppBot.py

--- WhatsAppBot.py
+++ WhatsAppBot.py
@@ -15,34 +15,36 @@ try:
     while(driver.find_element_by_class_name("_1QMFu")):
         print("Waiting 15s for login...")
         if time.time() > timeout:
-            print("Sorry login timed out..")
+            print("Sorry login timed out...")
             break
 except:
-    print("Waiting to 3s to load...\n----------ENTER CONTACT NAME----------")
-    contact_name = str(input())
-    print("----------ENTER THE NUMBER OF TIME YOU WANT TO SEND THE MESSAGE----------")
-    message_count = int(input().rstrip())
-    print("----------ENTER THE MESSAGE YOU WANT TO SEND----------")
-    message = str(input())
-    time.sleep(3)
+    pass
 
-    print("Locating searchbar...")
-    search_xpath = '//*[@id="side"]/div[1]/div/label/div/div[2]'
-    input_box_search = WebDriverWait(driver, 50).until(lambda driver: driver.find_element_by_xpath(search_xpath))
-    input_box_search.click()
+print("Waiting to 3s to load...\n----------ENTER CONTACT NAME----------")
+contact_name = str(input())
+print("----------ENTER THE NUMBER OF TIME YOU WANT TO SEND THE MESSAGE----------")
+message_count = int(input().rstrip())
+print("----------ENTER THE MESSAGE YOU WANT TO SEND----------")
+message = str(input())
+time.sleep(3)
 
-    print("Searching contact...")
-    input_box_search.send_keys(contact_name)
-    time.sleep(3)
-    selected_contact = driver.find_element_by_xpath("//span[@title='"+contact_name+"']")
-    selected_contact.click()
+print("Locating searchbar...")
+search_xpath = '//*[@id="side"]/div[1]/div/label/div/div[2]'
+input_box_search = WebDriverWait(driver, 50).until(lambda driver: driver.find_element_by_xpath(search_xpath))
+input_box_search.click()
 
-    print("Contact Located...")
-    inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
-    input_box = driver.find_element_by_xpath(inp_xpath)
+print("Searching contact...")
+input_box_search.send_keys(contact_name)
+time.sleep(3)
+selected_contact = driver.find_element_by_xpath("//span[@title='"+contact_name+"']")
+selected_contact.click()
 
-    print("Sending Message...")
-    for _ in range(message_count):
-        input_box.send_keys(message + Keys.ENTER)
+print("Contact Located...")
+inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+input_box = driver.find_element_by_xpath(inp_xpath)
+
+print("Sending Message...")
+for _ in range(message_count):
+    input_box.send_keys(message + Keys.ENTER)
     time.sleep(5)
     driver.quit()
